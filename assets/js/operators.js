3f9ef57ea2ad6b6daef97c7c9d5ad501cf2de5a6
Michael P <michpouls@gmail.com>
2023-11-16 11:30:17 +0100

    rjwpi

--- assets/js/operators.js
+++ assets/js/operators.js
@@ -5,23 +5,38 @@ const B = 30;
 const C = '30';
 const D = 10;
 const E = 40;
+let F
 
 // Opgave 1: skriv et expression der viser true når A er lig med B eller A er lig med D.
 
-if ( /*..dit expression Her*/ ) {
+if (A == B || A == D) {
     console.log('statement true!');
 }
 
 
 //opgave 2: skrive en If then statement der bliver TRUE når  A er større end B og A er mindre end E.
 
-
+if (A > B || A < E) {
+    console.log('statement true!');
+} else {
+    console.log('statement false!');
+}
 
 
 //opgave 3: Skriv en If then statement med  && og  || der kun er sand når alle expressions er sande.
 
-
+if (A === D && B == C || A <= B) {
+    console.log('sande!');
+} else {
+    console.log('ikke sande!');
+}
 
 
 
 //opgave 4: Skriv en If then statement der kan finde ud af om F er "undefined" eller har en værdi, ved at bruge !.
+
+if (!F) {
+    console.log('F is undefined');
+} else {
+    console.log('F har værdig!')
+}
